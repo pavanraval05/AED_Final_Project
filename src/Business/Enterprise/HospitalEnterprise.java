384ee6a6460a01f0604834f7b09f60633af2ebbf
pavan996066 <pavan996066@gmail.com>
2022-12-06 18:33:07 -0500

    Class

---
+++ src/Business/Enterprise/HospitalEnterprise.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business.Enterprise;
+
+import Business.Order.OrderHistory;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pavan
+ */
+
+//Initializing  class  HospitalEnterprise extends Organization
+public class HospitalEnterprise extends Enterprise {
+    
+    private OrderHistory orderHistory;
+    
+    public HospitalEnterprise(String name) {
+        
+        super(name, EnterpriseType.Hospital);
+        this.orderHistory = new OrderHistory();
+    }
+
+    public OrderHistory getOrderHistory() {
+        return orderHistory;
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
---
+++ src/Business/Enterprise/LocalHealthDepartment.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pavan
+ */
+
+//Initializing  class  LocalHealthDepartment extends Enterprise
+public class LocalHealthDepartment extends Enterprise {
+
+    public LocalHealthDepartment(String stateName) {
+        super(stateName, EnterpriseType.LocalHealthDepartment);
+    }
+
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
